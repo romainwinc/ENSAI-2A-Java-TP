2c60d0ae819fba593718235966a5687562c6b365
romaiw <romain.winckell@eleve.ensai.fr>
2025-04-07 09:02:04 +0000

    maj

--- tp4/src/main/java/fr/ensai/running/controller/ApiRestController.java
+++ tp4/src/main/java/fr/ensai/running/controller/ApiRestController.java
@@ -3,7 +3,13 @@ package fr.ensai.running.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -24,6 +30,31 @@ public class ApiRestController {
     public List<Athlete> allAthletes() {
 
         return athleteService.findAll();
+    }
+
+    @GetMapping("/athlete/{id}")
+    public ResponseEntity<Athlete> getAthleteById(@PathVariable Long id) {
+        Athlete athlete = athleteService.findById(id);
+        if (athlete == null) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(athlete);
+    }
+
+    @PostMapping("/athlete")
+    public ResponseEntity<Athlete> createUser(@RequestBody Athlete athlete) {
+        Athlete createdAthlete = athleteService.save(athlete);
+        return ResponseEntity.status(HttpStatus.CREATED).body(createdAthlete);
+    }
+
+    @DeleteMapping("/athlete/{id}")
+    public ResponseEntity<Void> deleteUser(@PathVariable Long id) {
+        Athlete existingAthlete = athleteService.findById(id);
+        if (existingAthlete == null) {
+            return ResponseEntity.notFound().build();
+        }
+        athleteService.deleteById(id);
+        return ResponseEntity.noContent().build();
     }
 
 }
